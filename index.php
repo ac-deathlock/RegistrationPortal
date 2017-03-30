e01849945ee4a4986b2442273b887dff70952407
Aayush Choudhry <choudhryaayush@gmail.com>
2017-03-31 00:20:08 +0530

    slider added

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<body background="download.jpg">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
 <h1 style="text-align: center;">Portal Infotsav</h1>
 <ul class="list-group" style="text-align: center;">
   <a href="regestration.php"><li class="list-group-item">Registeration</li>
@@ -6,4 +15,41 @@
   <a href="accomodation.php"><li class="list-group-item">Accomodation</li>
   
 </ul>
-</body>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="http://d2jj6pqz2mh6m6.cloudfront.net/event-images/79bc3978c1543a4eb0d41a796c6b5434.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="https://i.ytimg.com/vi/aDd7whvUPAA/sddefault.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="http://image.slidesharecdn.com/infotsav2013-140828130902-phpapp01/95/infotsav-2013-13-638.jpg%253Fcb%253D1409231387" alt="Flower">
+    </div>
+
+    <div class="item">
+      <img src="https://www.codechef.com/download/small-banner/CORU2015/1442065456.jpg" alt="Flower">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
